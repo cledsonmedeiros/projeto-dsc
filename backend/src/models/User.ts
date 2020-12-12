355cc997bca45d91abbb1b6df43cf812b21c02e7
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2020-12-12 17:46:52 -0300

    chore: add User model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,32 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from "typeorm";
+
+import * as bcrypt from "bcryptjs";
+
+@Entity()
+@Unique(["login"])
+export class Usuario extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  username!: string;
+
+  @Column()
+  password!: string;
+
+  passwordHash() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
+  passwordCheck(password: string) {
+    return bcrypt.compareSync(password, this.password);
+  }
+}
