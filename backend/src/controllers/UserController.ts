0dda5e1a12645d42cc8c73be8a1b9850c17e94da
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2020-12-12 19:54:23 -0300

    fix: UserController

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,6 +1,5 @@
 import { User } from "../models/User";
 import { AbstractController } from "./AbstractController";
-// import test from "../middlewares/test";
 
 export class UserController extends AbstractController {
 
@@ -8,8 +7,6 @@ export class UserController extends AbstractController {
 
   registerRoutes() {
     this.forRoute('/').get(this.index())
-
-    // this.setMiddleware(test)
 
     this.forRoute('/').post(this.create())
 
@@ -22,50 +19,73 @@ export class UserController extends AbstractController {
 
   index() {
     return async (req: any, res: any, next: any) => {
-      const users = await User.find({});
-      return res.json(users);
+      try {
+        const users = await User.find({});
+        return res.json(users);
+      } catch (error) {
+        return res.status(500).json({ msg: 'Erro interno no servidor', error })
+      }
     }
   }
 
   create() {
     return async (req: any, res: any, next: any) => {
-      const user: User = new User();
-      Object.assign(user, req.body)
-      user.passwordHash();
-      await user.save();
-      return res.json(user);
+      try {
+        const user: User = new User();
+        Object.assign(user, req.body)
+        user.passwordHash();
+        await user.save();
+        return res.json(user);
+      } catch (error) {
+        return res.status(500).json({ msg: 'Erro interno no servidor', error })
+      }
     }
   }
 
   show() {
     return async (req: any, res: any, next: any) => {
-      const user: User = await User.findOne({ id: req.params.id }) as User;
+      try {
+        const user: User = await User.findOne({ id: req.params.id }) as User;
 
-      if (!user) {
-        return res.status(404).json({ msg: 'Usuário não encontrado' })
+        if (!user) {
+          return res.status(404).json({ msg: 'Usuário não encontrado' })
+        }
+
+        return res.json(user);
+      } catch (error) {
+        return res.status(500).json({ msg: 'Erro interno no servidor', error })
       }
-
-      return res.json(user);
     }
   }
 
   update() {
     return async (req: any, res: any, next: any) => {
-      const user: User = await User.findOne({ id: req.params.id }) as User;
-      Object.assign(user, req.body)
-      await user.save();
-      return res.json(user);
+      try {
+        const user: User = await User.findOne({ id: req.params.id }) as User;
+        Object.assign(user, req.body)
+        if (req.body.password) {
+          user.passwordHash();
+        }
+        await user.save();
+        return res.json(user);
+      } catch (error) {
+        return res.status(500).json({ msg: 'Erro interno no servidor', error })
+      }
     }
   }
 
   delete() {
     return async (req: any, res: any, next: any) => {
-      const user: User = await User.findOne({ id: req.params.id }) as User;
-      if (!user) {
-        return res.status(404).json({ msg: 'Usuário não encontrado' })
+      try {
+        const user: User = await User.findOne({ id: req.params.id }) as User;
+        if (!user) {
+          return res.status(404).json({ msg: 'Usuário não encontrado' })
+        }
+        await user.remove();
+        return res.status(200).json({ msg: 'Usuário removido com sucesso' });
+      } catch (error) {
+        return res.status(500).json({ msg: 'Erro interno no servidor', error })
       }
-      await user.remove();
-      return res.status(200).json({ msg: 'Usuário removido com sucesso' });
     }
   }
 }
