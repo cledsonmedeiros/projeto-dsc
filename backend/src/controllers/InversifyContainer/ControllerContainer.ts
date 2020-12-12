668b2c56e1a1e51a040289732e68206011bcd715
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2020-12-12 18:19:20 -0300

    chore: register UserController

--- backend/src/controllers/InversifyContainer/ControllerContainer.ts
+++ backend/src/controllers/InversifyContainer/ControllerContainer.ts
@@ -1,10 +1,12 @@
 import { Container } from "inversify";
 import { IController } from "../IController";
 import { IndexController } from "../IndexController";
+import { UserController } from "../UserController";
 import ControllerTypes from "./ControllerTypes";
 
 const ControllerContainer = new Container();
 
 ControllerContainer.bind<IController>(ControllerTypes.Controller).to(IndexController);
+ControllerContainer.bind<IController>(ControllerTypes.Controller).to(UserController);
 
 export default ControllerContainer
