751f3948f24043d4213f4ea6839a4876c9aa9177
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2020-12-12 17:26:25 -0300

    chore: add app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,22 @@
+import "reflect-metadata";
+import express from "express";
+import logger from "morgan";
+import { IController } from "./controllers/IController";
+import ControllerContainer from "./controllers/InversifyContainer/ControllerContainer";
+import ControllerTypes from "./controllers/InversifyContainer/ControllerTypes";
+import { createConnection } from "typeorm";
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+createConnection();
+
+const controllers: IController[] = ControllerContainer.getAll<IController>(ControllerTypes.Controller);
+controllers.forEach(controller => {
+  controller.forApp(app).registerRoutes();
+});
+
+export default app;
