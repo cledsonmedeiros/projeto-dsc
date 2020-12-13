34c6ce3a234c69d02fb8d088d3959df6f41a129f
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2020-12-12 22:11:22 -0300

    chore: add onlyGustavo middleware

---
+++ backend/src/middlewares/onlyGustavo.ts
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+
+const { promisify } = require("util");
+
+export default async (req: any, res: any, next: any) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ msg: 'Token não fornecido' });
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    req.userId = decoded.userId;
+    if (decoded.userUsername !== 'gustavo') {
+      return res.status(403).json({ msg: 'Você não é o Gustavo' });
+    }
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ msg: 'Token inválido' });
+  }
+};
